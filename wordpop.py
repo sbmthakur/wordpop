d0b7d9b0ebccad9d780b932c206ca6f1aaa49144
shubham thakur <sbmthakur@gmail.com>
2017-07-28 18:48:38 +0530

    Display a sentence for the word
    For a word with no examples, "None" is displayed

--- wordpop.py
+++ wordpop.py
@@ -14,8 +14,16 @@ lexical_entries_length = len(word_data['results'][0]['lexicalEntries'])
 random_entry = random.randint(0, lexical_entries_length - 1)
 lexical_category = word_data['results'][0]['lexicalEntries'][random_entry]['lexicalCategory']
 definition = word_data['results'][0]['lexicalEntries'][random_entry]['entries'][0]['senses'][0]['definitions'][0]
+
+#Following try-except fetches an example sentence for the word
+try:
+    example_sentence = word_data['results'][0]['lexicalEntries'][random_entry]['entries'][0]['senses'][0]['examples'][0]['text']
+except LookupError:
+    #You will arrive here if there is no "text" key 
+    example_sentence = "None"
+
 word = word_data['results'][0]['word']
 
-cmd = '/usr/bin/notify-send "' + word + ' | ' + lexical_category + '" "' + definition + '"'
+cmd = '/usr/bin/notify-send "' + word + ' | ' + lexical_category + '" "' + definition + '\n<b>Ex:</b> ' + example_sentence + '"'
 print cmd
 os.system(cmd)
