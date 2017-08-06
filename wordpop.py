f7b43e5bc14f7fc03d085d695dbad4d910a21453
shubham thakur <sbmthakur@gmail.com>
2017-08-06 19:12:23 +0530

    Append synonym of the word in notification's titie

--- wordpop.py
+++ wordpop.py
@@ -6,6 +6,14 @@ import random
 
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+#Oxford API usually returns a list of synonyms
+#Here we are only returning the first one
+def fetch_synonym(synonyms):
+    if synonyms != "none":
+        return synonyms.split(',')[0]
+    else:
+        return None
+
 key = redis_client.randomkey()
 json_str = redis_client.get(key)
 word_data = json.loads(json_str)
@@ -24,6 +32,12 @@ except LookupError:
 
 word = word_data['results'][0]['word']
 
-cmd = '/usr/bin/notify-send "' + word + ' | ' + lexical_category + '" "' + definition + '\n<b>Ex:</b> ' + example_sentence + '"'
+synonym = fetch_synonym(word_data['synonyms'])
+
+#This is done fot the sake of formatting
+if synonym:
+    synonym = ' | ' + synonym
+
+cmd = '/usr/bin/notify-send "' + word + ' | ' + lexical_category + synonym + '" "' + definition + '\nEx: ' + example_sentence + '"'
 print cmd
 os.system(cmd)
